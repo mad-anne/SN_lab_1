20853e92a501bd2aa38cb0972c2c9a5ace2e3ab4
Anna Siwik <221026@student.pwr.edu.pl>
2017-10-06 10:20:03 +0200

    Init directory with sample files

---
+++ src/perceptron/header/Perceptron.h
@@ -0,0 +1,10 @@
+#ifndef SN_LAB_1_PERCEPTRON_H
+#define SN_LAB_1_PERCEPTRON_H
+
+
+class Perceptron {
+
+};
+
+
+#endif //SN_LAB_1_PERCEPTRON_H
---
+++ src/perceptron/source/Perceptron.cpp
@@ -0,0 +1,1 @@
+#include "perceptron/header/Perceptron.h"
